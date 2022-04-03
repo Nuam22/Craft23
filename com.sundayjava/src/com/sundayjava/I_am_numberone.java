3b6217d2a7dc8b26a662d7886f33d70c00ac3711
Nuamin <markonal15@gmail.com>
2022-04-03 16:53:08 -0400

    secondtimetesting

---
+++ com.sundayjava/src/com/sundayjava/I_am_numberone.java
@@ -0,0 +1,15 @@
+package com.sundayjava;
+
+public class I_am_numberone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 3;
+		int b = 3;
+		
+		int medemer = a+b;
+		System.out.println(medemer);
+	}
+
+}
